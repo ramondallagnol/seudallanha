393d00d743912c1a0cef2d4fb3b46dffe04b1a03
ramondallagnol <ramon_silveira15@hotmail.com>
2020-07-17 11:08:19 -0300

    Update ProdutosApiApplication

--- src/main/java/com/seudallanha/ProdutosApi/ProdutosApiApplication.java
+++ src/main/java/com/seudallanha/ProdutosApi/ProdutosApiApplication.java
@@ -2,8 +2,9 @@ package com.seudallanha.ProdutosApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class ProdutosApiApplication {
 
 	public static void main(String[] args) {
